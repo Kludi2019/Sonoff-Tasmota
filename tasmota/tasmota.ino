adf11f44b1c166b208683ba621d1690e65b126ae
gemu2015 <gmutz2010@googlemail.com>
2020-05-12 19:11:31 +0200

    fix sml script

--- tasmota/tasmota.ino
+++ tasmota/tasmota.ino
@@ -347,9 +347,7 @@ void SleepDelay(uint32_t mseconds) {
   if (mseconds) {
     for (uint32_t wait = 0; wait < mseconds; wait++) {
       delay(1);
-      if (!serial_local) {
-        if (Serial.available()) { break; }  // We need to service serial buffer ASAP as otherwise we get uart buffer overrun
-      }
+      if (Serial.available()) { break; }  // We need to service serial buffer ASAP as otherwise we get uart buffer overrun
     }
   } else {
     delay(0);
